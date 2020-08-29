348f3b5be8ff9181770875d676cd80f6d51bbd4e
Daniel Kawakami <daniel.kawakami.en@gmail.com>
2020-08-29 12:31:36 -0300

    Aula 37 - Adicao de body-parser

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Informo o Express o ejs como view engine
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+// Linkando body-parser no express - necessario para receber parametros do POST
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
+// Rotas
 app.get("/", (req, res)=>{
     res.render("index")
 })
 
 app.get("/perguntar", (req, res)=>{
     res.render("perguntar");
+})
+
+app.post("/salvarpergunta", (req, res)=>{
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    res.send("Formulario recebido, titulo: " + titulo + " descricao: " + descricao);
 })
 
 app.listen(8080, ()=>{
